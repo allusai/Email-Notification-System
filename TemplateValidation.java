8fd41c8503152977b16d664f54f8e773be3157af
Sai Allu <allu@usc.edu>
2017-11-07 19:59:45 -0800

    Update TemplateValidation.java

--- TemplateValidation.java
+++ TemplateValidation.java
@@ -1,9 +1,46 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.String;
 
 
 public class TemplateValidation {
+	private File thisFile;
+	
+	//Constructor
+	public TemplateValidation(File f)
+	{
+		thisFile = f;
+	}
+	
+	
+	/** Checks if the data in thisFile is what we need to send Notification1. Later
+	* on we can implement more comprehensive checking.
+	*
+	* @return boolean  Whether all the data is valid 
+	*/
+	boolean validate1()
+	{
+
+	}
+	
+	/** Checks if the data in thisFile is what we need to send Notification2.
+	*
+	* @return boolean  Whether all the data is valid 
+	*/
+	boolean validate2()
+	{
+
+	}
+	
+	/** Checks if the data in thisFile is what we need to send Notification3.
+	*
+	* @return boolean  Whether all the data is valid 
+	*/
+	boolean validate3()
+	{
+
+	}
 	
 	public static void main(String[] args) throws FileNotFoundException
 	{
@@ -12,7 +49,7 @@ public class TemplateValidation {
 		Scanner fileNameInput = new Scanner(System.in);
 		String fileName = "";
 		
-		System.out.println("Enter the name of a file");
+		System.out.println("Enter the name of a file: ");
 		fileName = fileNameInput.nextLine();
 		
 		//Create a File object that we can parse
@@ -22,10 +59,12 @@ public class TemplateValidation {
 		Scanner fileScanner = new Scanner(inputFile);
 		System.out.println("File not found");
 		
-		//Check what the first number in the file is
-		int command;
-		command = fileScanner.nextInt();
+		//Check what the first two letters of the file are (the type of notification)
+		String command;
+		command = fileName.substring(0,2);
 		
+		/* See if the rest of the data provided in that file is what we expected 
+		for that type of notification */
 		boolean validData = false;
 		
 		if(command == 0)
