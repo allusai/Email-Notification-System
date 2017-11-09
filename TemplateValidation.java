15a5522951b5f91d8069033b6f5931227a8afd7a
Sai Allu <allu@usc.edu>
2017-11-08 21:58:54 -0800

    Add files via upload

--- TemplateValidation.java
+++ TemplateValidation.java
@@ -13,6 +13,24 @@ public class TemplateValidation {
 		thisFile = f;
 	}
 	
+	/** Regardless of the notification, the first line always contains the
+	* email address of the recipient. So this data can be validated in a separate
+	* chunk of code (document)
+	*
+	* @return boolean  Whether the first line is a valid email address
+	*/
+	boolean validateEmail()
+	{
+		Scanner fileScanner = new Scanner(thisFile);
+		
+		/* The first line should be a string with an @ and . */
+		String firstLine = fileScanner.nextLine();
+		
+		if(firstLine.indexOf('@') == -1) //If '@' not found, invalid
+			return false;
+		if(firstLine.indexOf('.') == -1) //If '.' not found, invalid
+			return false;
+	}
 	
 	/** Checks if the data in thisFile is what we need to send Notification1. Later
 	* on we can implement more comprehensive checking.
@@ -21,7 +39,29 @@ public class TemplateValidation {
 	*/
 	boolean validate1()
 	{
-
+		Scanner fileScanner = new Scanner(thisFile);
+		
+		//If the email is not valid, return false
+		if(!validateEmail())
+			return false;
+		
+		//Note that we can jump over the first line inside this method (document)
+		fileScanner.nextLine();
+		
+		/* The next line should be a string between 2 and 40 characters */
+		String secondLine = fileScanner.nextLine();
+		
+		if(secondLine.length() < 2 || secondLine.length() > 40)
+			return false;
+			
+		/* The next line should be a double between 0.0 and 100.0 */
+		double grade = fileScanner.nextDouble();
+		
+		if(grade < 0.0 || grade > 100.0)
+			return false;
+			
+		//If we did all the checks and there is no problem, the file is valid
+		return true;
 	}
 	
 	/** Checks if the data in thisFile is what we need to send Notification2.
@@ -55,13 +95,10 @@ public class TemplateValidation {
 		//Create a File object that we can parse
 		File inputFile = new File(fileName);
 		
-		//Scanner to parse the file
-		Scanner fileScanner = new Scanner(inputFile);
-		System.out.println("File not found");
-		
 		//Check what the first two letters of the file are (the type of notification)
 		String command;
 		command = fileName.substring(0,2);
+		
 		
 		/* See if the rest of the data provided in that file is what we expected 
 		for that type of notification */
