2b5bd7caffe37639f389c47d9dcf685d061fc0b3
Sai Allu <allu@usc.edu>
2017-11-10 12:16:20 -0800

    Update TemplateValidation.java

--- TemplateValidation.java
+++ TemplateValidation.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.String;
 
 
 public class TemplateValidation {
@@ -18,8 +17,9 @@ public class TemplateValidation {
 	* chunk of code (document)
 	*
 	* @return boolean  Whether the first line is a valid email address
+	 * @throws FileNotFoundException 
 	*/
-	boolean validateEmail()
+	boolean validateEmail() throws FileNotFoundException
 	{
 		Scanner fileScanner = new Scanner(thisFile);
 		
@@ -30,14 +30,18 @@ public class TemplateValidation {
 			return false;
 		if(firstLine.indexOf('.') == -1) //If '.' not found, invalid
 			return false;
+		
+		//If all of that is good, return true
+		return true;
 	}
 	
 	/** Checks if the data in thisFile is what we need to send Notification1. Later
 	* on we can implement more comprehensive checking.
 	*
 	* @return boolean  Whether all the data is valid 
+	 * @throws FileNotFoundException 
 	*/
-	boolean validate1()
+	boolean validate1() throws FileNotFoundException
 	{
 		Scanner fileScanner = new Scanner(thisFile);
 		
@@ -70,7 +74,7 @@ public class TemplateValidation {
 	*/
 	boolean validate2()
 	{
-
+		return true;
 	}
 	
 	/** Checks if the data in thisFile is what we need to send Notification3.
@@ -79,39 +83,7 @@ public class TemplateValidation {
 	*/
 	boolean validate3()
 	{
-
+		return true;
 	}
 	
-	public static void main(String[] args) throws FileNotFoundException
-	{
-		
-		//Scanner to take in the name of the file
-		Scanner fileNameInput = new Scanner(System.in);
-		String fileName = "";
-		
-		System.out.println("Enter the name of a file: ");
-		fileName = fileNameInput.nextLine();
-		
-		//Create a File object that we can parse
-		File inputFile = new File(fileName);
-		
-		//Check what the first two letters of the file are (the type of notification)
-		String command;
-		command = fileName.substring(0,2);
-		
-		
-		/* See if the rest of the data provided in that file is what we expected 
-		for that type of notification */
-		boolean validData = false;
-		
-		if(command == 0)
-		{
-			validData = validate1(inputFile);
-		}
-			
-		
-		
-		
-	}
-
 }
