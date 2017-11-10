d669f4c323d555912e668f38185fdde6cb546e76
Sai Allu <allu@usc.edu>
2017-11-10 12:15:35 -0800

    Update EmailRunner.java

--- EmailRunner.java
+++ EmailRunner.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.String;
 
 public class EmailRunner {
 
@@ -15,13 +14,15 @@ public class EmailRunner {
 		System.out.println("Enter the name of a file: ");
 		fileName = fileNameInput.nextLine();
 		
+		System.out.println("You entered: " + fileName);
+		
 		//Create a File object that we can parse
 		File inputFile = new File(fileName);
 		
 		//Check what the first two letters of the file are (the type of notification)
 		String command;
 		command = fileName.substring(0,2);
-		
+		System.out.println("Command was: " + command);
 		
 		/* See if the rest of the data provided in that file is what we expected 
 		for that type of notification */
@@ -30,8 +31,9 @@ public class EmailRunner {
 		
 		if(command.equals("N1"))
 		{
+			System.out.println("Inside N1 validation");
 			validData = tempValid.validate1();
 		}
-			
+
 	}
 }
