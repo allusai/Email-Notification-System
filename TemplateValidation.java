7706ff9b54a410e9deac3ce07d2ac7f5f3528dbb
Sai Allu <allu@usc.edu>
2017-11-11 15:10:33 -0800

    Update TemplateValidation.java

--- TemplateValidation.java
+++ TemplateValidation.java
@@ -71,9 +71,43 @@ public class TemplateValidation {
 	/** Checks if the data in thisFile is what we need to send Notification2.
 	*
 	* @return boolean  Whether all the data is valid 
+	 * @throws FileNotFoundException 
 	*/
-	boolean validate2()
+	boolean validate2() throws FileNotFoundException
 	{
+		Scanner fileScanner = new Scanner(thisFile);
+		
+		//If the email is not valid, return false
+		if(!validateEmail())
+			return false;
+		
+		//Note that we can jump over the first line inside this method (document)
+		fileScanner.nextLine();
+		
+		/* The next line should be a day of the week */
+		String secondLine = fileScanner.nextLine();
+		boolean validDayOfWeek = isValidDay(secondLine); //See bottom of code
+		
+		//If not a valid day of the week, return false
+		if(!validDayOfWeek)
+			return false;
+			
+		/* The next line should be a MM/DD */
+		String thirdLine = fileScanner.nextLine();
+		boolean validDate = isValidDate(thirdLine); //See below
+		
+		if(!validDate)
+			return false;
+		
+		/* The next line should be a valid time like 8:30PM */
+		String fourthLine = fileScanner.nextLine();
+		boolean validTime = isValidTime(fourthLine); //See helper method below
+		
+		if(!validTime)
+			return false;
+		
+			
+		//If we did all the checks and there is no problem, the file is valid
 		return true;
 	}
 	
@@ -85,5 +119,71 @@ public class TemplateValidation {
 	{
 		return true;
 	}
+	
+	
+	
+	
+	//I noticed that a lot of the time I was validating the
+	//format of the same info in multiple notification files.
+	//So I made these helper methods to be able to re-use that code.
+	
+	
+	//Checks if the string passed in is a day from Monday-Sunday 
+	boolean isValidDay(String s)
+	{
+		String[] validDays = {"Monday","Tuesday","Wednesday",
+				"Thursday","Friday","Saturday","Sunday"};
+		boolean isValidDay = false;
+		
+		//Loop through all possible days and see if second line is one of them
+		for(int i = 0; i < validDays.length; i++)
+		{
+			if(s.equals(validDays[i]))
+				isValidDay = true;
+		}
+		
+		//Return whether the string is a valid day
+		return isValidDay;
+	}
+	
+	//Checks if string passed in is a date in the format MM/DD
+	boolean isValidDate(String s)
+	{
+		//MM
+		int mm = Integer.parseInt(s.substring(0,2));
+				
+		//DD
+		int dd = Integer.parseInt(s.substring(3,5));
+				
+		//If mm is not between 1 and 12, return false
+		if(mm < 1 || mm > 12)
+			return false;
+		else if(dd < 1 || dd > 31)
+			return false;
+		else if(s.charAt(2) != '/')
+			return false;
+		
+		return true;
+	}
+	
+	//Checks if string passed is a valid time like 8:30PM
+	boolean isValidTime(String s)
+	{
+		//The last two characters need to be AM or PM
+		String lastTwoChars = s.substring(s.length()-2,s.length());
+		
+		if(!lastTwoChars.equals("AM") && !lastTwoChars.equals("PM"))
+			return false;
+		
+		//There needs to be a colon at index 1 (8:30) or index 2 (12:30)
+		//If the location of the ":" is not 1 and is not 2, then this isn't a valid time
+		if(s.indexOf(':') != 1 && s.indexOf(':') != 2)
+			return false;
+		
+		return true;
+	}
+	
+	
+	
 	
 }
