19db2a26de3c94a817885426b7a50f35edc8d19b
Sai Allu <allu@usc.edu>
2017-11-12 13:53:21 -0800

    Update TemplateValidation.java

--- TemplateValidation.java
+++ TemplateValidation.java
@@ -12,6 +12,11 @@ public class TemplateValidation {
 		thisFile = f;
 	}
 	
+	public TemplateValidation() 
+	{
+		thisFile = null;
+	}
+
 	/** Regardless of the notification, the first line always contains the
 	* email address of the recipient. So this data can be validated in a separate
 	* chunk of code (document)
@@ -19,16 +24,14 @@ public class TemplateValidation {
 	* @return boolean  Whether the first line is a valid email address
 	 * @throws FileNotFoundException 
 	*/
-	boolean validateEmail() throws FileNotFoundException
+	boolean validateEmail(String s) throws FileNotFoundException
 	{
-		Scanner fileScanner = new Scanner(thisFile);
 		
-		/* The first line should be a string with an @ and . */
-		String firstLine = fileScanner.nextLine();
+		/* The string should have an @ and . */
 		
-		if(firstLine.indexOf('@') == -1) //If '@' not found, invalid
+		if(s.indexOf('@') == -1) //If '@' not found, invalid
 			return false;
-		if(firstLine.indexOf('.') == -1) //If '.' not found, invalid
+		if(s.indexOf('.') == -1) //If '.' not found, invalid
 			return false;
 		
 		//If all of that is good, return true
@@ -46,11 +49,10 @@ public class TemplateValidation {
 		Scanner fileScanner = new Scanner(thisFile);
 		
 		//If the email is not valid, return false
-		if(!validateEmail())
-			return false;
+		String firstLine = fileScanner.nextLine();
 		
-		//Note that we can jump over the first line inside this method (document)
-		fileScanner.nextLine();
+		if(!validateEmail(firstLine))
+			return false;
 		
 		/* The next line should be a string between 2 and 40 characters */
 		String secondLine = fileScanner.nextLine();
@@ -78,11 +80,10 @@ public class TemplateValidation {
 		Scanner fileScanner = new Scanner(thisFile);
 		
 		//If the email is not valid, return false
-		if(!validateEmail())
+		String firstLine = fileScanner.nextLine();
+				
+		if(!validateEmail(firstLine))
 			return false;
-		
-		//Note that we can jump over the first line inside this method (document)
-		fileScanner.nextLine();
 		
 		/* The next line should be a day of the week */
 		String secondLine = fileScanner.nextLine();
@@ -121,11 +122,10 @@ public class TemplateValidation {
 		Scanner fileScanner = new Scanner(thisFile);
 		
 		//If the email is not valid, return false
-		if(!validateEmail())
+		String firstLine = fileScanner.nextLine();
+				
+		if(!validateEmail(firstLine))
 			return false;
-		
-		//Note that we can jump over the first line inside this method (document)
-		fileScanner.nextLine();
 		
 		/* The next line should be a string between 2 and 40 characters */
 		String secondLine = fileScanner.nextLine();
@@ -181,6 +181,7 @@ public class TemplateValidation {
 	//Checks if string passed in is a date in the format MM/DD
 	boolean isValidDate(String s)
 	{
+		try{
 		//MM
 		int mm = Integer.parseInt(s.substring(0,2));
 				
@@ -193,7 +194,11 @@ public class TemplateValidation {
 		else if(dd < 1 || dd > 31)
 			return false;
 		else if(s.charAt(2) != '/')
+			return false; }
+		catch(Exception e)
+		{
 			return false;
+		}
 		
 		return true;
 	}
@@ -201,7 +206,16 @@ public class TemplateValidation {
 	//Checks if string passed is a valid time like 8:30PM
 	boolean isValidTime(String s)
 	{
+		try{
+		//Handles empty string case
+		if(s.length() < 6)
+			return false;
+		
 		//The last two characters need to be AM or PM
+		//This led to a bug for the empty string case since
+		//s.length()-2 would be an out of bounds index. So the above
+		//code is a fix to handle strings that are too short to be
+		//valid times.
 		String lastTwoChars = s.substring(s.length()-2,s.length());
 		
 		if(!lastTwoChars.equals("AM") && !lastTwoChars.equals("PM"))
@@ -211,7 +225,13 @@ public class TemplateValidation {
 		//If the location of the ":" is not 1 and is not 2, then this isn't a valid time
 		if(s.indexOf(':') != 1 && s.indexOf(':') != 2)
 			return false;
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
 		
 		return true;
 	}
+	
 }
