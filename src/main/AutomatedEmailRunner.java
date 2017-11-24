7f78a05ed34d55dfb42cf6b005211872b035cbd6
Sai Allu <allu@usc.edu>
2017-11-24 14:32:43 -0800

    Update AutomatedEmailRunner.java

--- src/main/AutomatedEmailRunner.java
+++ src/main/AutomatedEmailRunner.java
@@ -1,7 +1,5 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.Scanner;
@@ -23,6 +21,12 @@ public class AutomatedEmailRunner {
 			return;
 		}
 		
+		/* We have the files we want to send in a directory. Once we send these files though, the
+		   program should not stop running since more files will be added to the directory (the directory
+		   is a queue to which files will keep be added to). To accomplish this, I used a while(true)
+		   infinite loop which will run until we tell the program to stop by adding a special
+		   file to the directory called "notification.quit." */
+		
 		while(true) {
 			
 			//Go to the directory specified
@@ -41,7 +45,7 @@ public class AutomatedEmailRunner {
 			for (File inputFile : directoryListing) 
 			{
 				String fileName = inputFile.getName();
-			//	System.out.println(fileName);
+				System.out.println(fileName);
 				
 				//When we want the program to stop processing the files
 				//in the directory, we will drop a special file into the
@@ -74,10 +78,7 @@ public class AutomatedEmailRunner {
 						
 					if(command.equals(p.getProperty("template1")))
 					{
-						//System.out.println(validData); //Before it is not valid
-						//System.out.println("Inside N1 validation");
 						validData = tempValid.validate1();
-						//System.out.println(validData); //Now it is valid
 					}
 			
 					else if(command.equals(p.getProperty("template2")))
@@ -99,44 +100,11 @@ public class AutomatedEmailRunner {
 					if(!validData)
 					{
 						System.out.println("Data is invalid.");
-						//return;
 					}
 					
-					if(command.equals(p.getProperty("template1")))
-					{
-						MessageCreator mc = new MessageCreator();
-						String message =  mc.customMessage1(inputFile);
-						
-						SendEmail sender = new SendEmail(message);
-						String recepient = "sai.2000k@gmail.com";
-						String subject = "Hello";
-						sender.sendTheMessage(p, subject,recepient);
-						
-					}
-			
-					else if(command.equals(p.getProperty("template2")))
-					{
-						MessageCreator mc = new MessageCreator();
-						String message =  mc.customMessage2(inputFile);
-							
-						SendEmail sender = new SendEmail(message);
-						String recepient = "sai.2000k@gmail.com";
-						String subject = "Hello";
-						sender.sendTheMessage(p, subject,recepient);
-						
-					}
-			
-					else if(command.equals(p.getProperty("template3")))
-					{
-						MessageCreator mc = new MessageCreator();
-						String message =  mc.customMessage3(inputFile);
-						
-						SendEmail sender = new SendEmail(message);
-						String recepient = "sai.2000k@gmail.com";
-						String subject = "Hello";
-						sender.sendTheMessage(p, subject,recepient);	
-					}
-			
+					//See the helper method below main
+					sendMessage(p,inputFile,command);
+					
 					//Now move that file into a different directory so it
 					//is not processed again
 			
@@ -153,15 +121,87 @@ public class AutomatedEmailRunner {
 					else{
 						System.out.println("File move failed");
 					} 
-		
+					
+					System.out.println();
 				}
 			} 
 			
+			/* We don't want to continuously process the files because that will waste
+			 * a lot of computing power. We only scan through the directory and check for
+			 * new files every 5 seconds. To build this functionality, we have to use the
+			 * Thread class which can put a thread to sleep for a given amount of time in milliseconds. */
 			try {
 				Thread.sleep(5000);
 			} catch (InterruptedException e) {
 				System.out.println("Sleep command did not work");
 			}
+			
+			
+			
 		} //While loop brace
 	}
+	
+	
+	
+	/** This helper method sends the actual email after all of the data has
+	 * been verified inside of main.
+	 * 
+	 * @param p  The Properties file containing login information for your Gmail account (username, password)
+	 * @param inputFile  The file that needs to be sent
+	 * @param command  The indicator of what type of notification you are sending
+	 * @throws FileNotFoundException
+	 * @throws MessagingException
+	 */
+	private static void sendMessage(Properties p, File inputFile, String command) throws FileNotFoundException, MessagingException
+	{
+		/* Once the data has been validated, figure out which notification we want to send and build the email */
+		
+		if(command.equals(p.getProperty("template1")))
+		{
+			//The MessageCreator builds the body of the email (the custom notification text)
+			MessageCreator mc = new MessageCreator();
+			String message =  mc.customMessage1(inputFile);
+			
+			//Initialize a SendEmail object which uses HTTP requests to actually build the
+			//email we want to send as a draft.
+			SendEmail sender = new SendEmail(message);
+			String subject = "Grade Notification";
+			
+			//We use a Scanner object to read in the recipient's email from the file
+			Scanner fileScanner = new Scanner(inputFile);
+			String recepient = fileScanner.nextLine();
+			
+			//This is the method from the JavaMail API that actually sends the email to the student
+			sender.sendTheMessage(p, subject,recepient);
+		}
+
+		else if(command.equals(p.getProperty("template2")))
+		{
+			MessageCreator mc = new MessageCreator();
+			String message =  mc.customMessage2(inputFile);
+				
+			SendEmail sender = new SendEmail(message);
+			String subject = "Class Reminder";
+			
+			Scanner fileScanner = new Scanner(inputFile);
+			String recepient = fileScanner.nextLine();
+			
+			sender.sendTheMessage(p, subject,recepient);	
+		}
+
+		else if(command.equals(p.getProperty("template3")))
+		{
+			MessageCreator mc = new MessageCreator();
+			String message =  mc.customMessage3(inputFile);
+			
+			SendEmail sender = new SendEmail(message);
+			String subject = "Missing Assignment";
+			
+			Scanner fileScanner = new Scanner(inputFile);
+			String recepient = fileScanner.nextLine();
+			
+			sender.sendTheMessage(p, subject,recepient);
+		}
+	}
+	
 }
